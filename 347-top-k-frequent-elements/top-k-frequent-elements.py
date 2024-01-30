ae4c6446f7f06fb24499743643074451d8b6aa67
Vignesh Baskaran <vignesh865@gmail.com>
2024-01-30 06:04:08 -0500

    Time: 85 ms (87.97%) | Memory: 21 MB (86.94%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,29 +1,20 @@
 import heapq
 
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        def new_cmp_lt(a, b):
+            return a[1] < b[1]
 
-        def new_cmp_lt(a,b):
-            return a[1]<b[1]
-
-#override the existing "cmp_lt" module function with your function
-        heapq.cmp_lt=new_cmp_lt
+        # # override the existing "cmp_lt" module function with your function
+        # heapq.cmp_lt = new_cmp_lt
 
         freq = {}
 
         for num in nums:
-
             freq[num] = freq.get(num, 0) + 1
 
         h = []
 
-        # for i in freq.items():
-        #     heapq.heappush(h, (i[1], i[0]))
-        # return [heapq.heappop(h)[1] for _ in range(k)]
-
-        # k_largest = heapq.nlargest(k, freq,  key = lambda item: item[1])
-        k_largest = heapq.nlargest(k, freq,  key = freq.get)
-
-        # print(k_largest)
+        k_largest = heapq.nlargest(k, freq, key=freq.get)
         return k_largest
-        
