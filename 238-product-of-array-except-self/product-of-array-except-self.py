b8573ae0f4c4636ff491ebb9e16bd49bf377277c
Vignesh Baskaran <vignesh865@gmail.com>
2024-01-30 06:42:55 -0500

    Time: 945 ms (5.01%) | Memory: 33.7 MB (6.15%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,30 +1,25 @@
 class Solution:
-
     def forward_sum(self, nums, i, sum_array):
-
-        if i == len(nums)-1:
+        if i == len(nums) - 1:
             sum_array.append(1)
             return 1
-        
-        sum = self.forward_sum(nums, i+1, sum_array)* nums[i+1]
+
+        sum = self.forward_sum(nums, i + 1, sum_array) * nums[i + 1]
         sum_array.insert(0, sum)
         return sum
 
     def reverse_sum(self, nums, i, sum_array):
-
         if i == 0:
-            sum_array[i] = sum_array[i]*1
+            sum_array[i] = sum_array[i] * 1
             return 1
-        
-        sum = self.reverse_sum(nums, i-1, sum_array)* nums[i-1]
-        sum_array[i] = sum_array[i]*sum
+
+        sum = self.reverse_sum(nums, i - 1, sum_array) * nums[i - 1]
+        sum_array[i] = sum_array[i] * sum
         return sum
 
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-
         forward_sum_arr = []
         self.forward_sum(nums, 0, forward_sum_arr)
-        reverese_sum_arr = []
-        self.reverse_sum(nums, len(nums)-1, forward_sum_arr)
+        self.reverse_sum(nums, len(nums) - 1, forward_sum_arr)
 
         return forward_sum_arr
